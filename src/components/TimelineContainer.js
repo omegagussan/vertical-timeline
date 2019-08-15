c4d5f499dc859eb19512c12fcc923cd607e02440
Gustav Sandström <gustav.sandstrom@schibsted.com>
2019-08-15 15:03:08 +0200

    better layout

--- src/components/TimelineContainer.js
+++ src/components/TimelineContainer.js
@@ -14,21 +14,9 @@ class TimelineContainer extends React.Component{
         );
         const sorted_data = sortable_data.sort(
             (a,b) => (a.date < b.date) ? 1 : ((b.date > a.date) ? -1 : 0)
-        ).reverse();
+        );
         const timespan = sorted_data[0].date - sorted_data[sorted_data.length-1].date;
-        const heights = [];
-        for (let i = 0; i < sorted_data.length -1; i++){
-            let diff = sorted_data[i+1].date - sorted_data[i].date;
-            let rel_diff = Math.abs(diff/timespan);
-            heights.push(rel_diff);
-        }
-        let augmented_data = sorted_data.map((e, i) => {
-            return ({...e,
-                height: heights[i]
-            })
-        });
-        augmented_data[augmented_data.length-1].height = 0;
-        console.log(augmented_data);
+        const augmented_data = this.augment_data_with_heights(sorted_data, timespan);
         const Timeline = () =>
             augmented_data.length > 0 && (
                 <div className="timeline-container">
@@ -40,7 +28,33 @@ class TimelineContainer extends React.Component{
         return(<Timeline/>)
     }
 
+    augment_data_with_heights(sorted_data, timespan) {
+        const heights = this.get_heights(sorted_data, timespan);
+        let augmented_data = sorted_data.map((e, i) => {
+            return ({
+                ...e,
+                height: heights[i]
+            })
+        });
+        augmented_data[augmented_data.length-1].height = 0;
+        return augmented_data;
+    }
 
+    get_heights(sorted_data, timespan) {
+        const heights = [];
+        let time_granularity = Number.MAX_SAFE_INTEGER;
+        for (let i = 0; i < sorted_data.length - 1; i++) {
+            let diff = sorted_data[i + 1].date - sorted_data[i].date;
+            let rel_diff = Math.abs(diff / timespan);
+            console.log(rel_diff);
+            if (rel_diff > 0  && rel_diff < time_granularity) {
+                time_granularity = rel_diff;
+            }
+            heights.push(rel_diff);
+        }
+        const total_pixel_length = 1/time_granularity * 220;
+        return heights.map((h) => h * total_pixel_length);
+    }
 }
 
 export default TimelineContainer;
--- src/components/TimelineItem.js
+++ src/components/TimelineItem.js
@@ -5,20 +5,13 @@ class TimelineItem extends React.Component {
 
     render() {
         const data = this.props.data;
-        console.log(data);
-        let body = document.body,
-            html = document.documentElement;
-
-        let height = Math.max(body.scrollHeight, body.offsetHeight,
-            html.clientHeight, html.scrollHeight, html.offsetHeight);
-        console.log(typeof data.date);
+        let height = this.pixel_view_height();
         const dateString = data.date.toLocaleDateString("en-US");
+        const tag = data.category && data.category.color && data.category.tag ? <span className="tag" style={{background: data.category.color}}>{data.category.tag}</span> : <div></div>;
         return (
-            <div className="timeline-item" style={{height: data.height * height * 0.7}}>
+            <div className="timeline-item" style={{height: data.height}}>
                 <div className="timeline-item-content">
-                <span className="tag" style={{background: data.category.color}}>
-                    {data.category.tag}
-                </span>
+                    {tag}
                     <time>{dateString}</time>
                     <p>{data.text}</p>
                     {data.link && (
@@ -33,6 +26,14 @@ class TimelineItem extends React.Component {
                     <span className="circle"/>
                  </div>
             </div>);
+    }
+
+    pixel_view_height() {
+        let body = document.body,
+            html = document.documentElement;
+
+        return Math.max(body.scrollHeight, body.offsetHeight,
+            html.clientHeight, html.scrollHeight, html.offsetHeight);
     }
 }
 
